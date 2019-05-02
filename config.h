5cbff98d6a6b5d0a3feaa17b991b2800f16ef263
Felipe Graeff <felipe.graeff@inf.ufrgs.br>
2019-05-02 05:45:45 -0300

    first commit

---
+++ config.h
@@ -0,0 +1,6 @@
+/************************ Adafruit IO Config *******************************/
+
+// visit io.adafruit.com if you need to create an account,
+// or if you need your Adafruit IO key.
+#define IO_USERNAME    "user_name"
+#define IO_KEY         "key"
---
+++ wifi_switch.ino
@@ -0,0 +1,101 @@
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+
+#include <ESP8266WebServer.h>
+#include <DNSServer.h>
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+
+#include "AdafruitIO_WiFi.h"
+// config.h must define the macros IO_USERNAME and IO_KEY
+// respectively your Adafruit IO username and key
+#include "config.h"
+
+
+// select which pin will trigger the configuration portal when set to LOW
+// ESP-01 users please note: the only pins available (0 and 2), are shared 
+// with the bootloader, so always set them HIGH at power-up
+#define BUTTON_PIN 1
+#define RELAY_PIN 2
+
+int push_count = 0;
+int light_state = LOW;
+bool toggle = false;
+bool configure_wifi = false;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("\n Starting");
+
+  pinMode(BUTTON_PIN, INPUT);
+  pinMode(RELAY_PIN, OUTPUT);
+}
+
+
+void loop() {
+  // control press time
+  if (digitalRead(BUTTON_PIN) == HIGH) {
+    push_count += 1;
+    configure_wifi = false;
+    toggle = false;
+  } else {
+    if (push_count > 0) {
+      if (push_count > 50) {
+        configure_wifi = true;
+      } else {
+        toggle = true;
+      }
+    }
+    push_count = 0;
+  }
+  
+  // is configuration portal requested?
+  if (configure_wifi) {
+    //WiFiManager
+    //Local intialization. Once its business is done, there is no need to keep it around
+    WiFiManager wifiManager;
+
+    //reset settings - for testing
+    //wifiManager.resetSettings();
+
+    //sets timeout until configuration portal gets turned off
+    //useful to make it all retry or go to sleep
+    //in seconds
+    //wifiManager.setTimeout(120);
+
+    //it starts an access point with the specified name
+    //here  "wifi_switch"
+    //and goes into a blocking loop awaiting configuration
+
+    //WITHOUT THIS THE AP DOES NOT SEEM TO WORK PROPERLY WITH SDK 1.5 , update to at least 1.5.1
+    //WiFi.mode(WIFI_STA);
+    
+    if (!wifiManager.startConfigPortal("wifi_switch")) {
+      Serial.println("failed to connect and hit timeout");
+      delay(3000);
+      //reset and try again, or maybe put it to deep sleep
+      ESP.reset();
+      delay(5000);
+    }
+
+    //if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+    
+    AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WiFi.SSID().c_str(), WiFi.psk().c_str());
+    AdafruitIO_Feed* digital = io.feed("switch");
+
+    
+  }
+
+
+  if (toggle) {
+    light_state = !light_state;
+    digitalWrite(RELAY_PIN, light_state);
+    toggle = false;
+  }
+  if (configure_wifi) {
+    Serial.println("resetting");
+    configure_wifi = false;
+  }
+
+}
