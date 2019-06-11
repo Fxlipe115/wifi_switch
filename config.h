4c209cf58dba911ca2e95f47d59fe7fb34a0a4c7
Felipe Graeff <felipe.graeff@inf.ufrgs.br>
2019-06-10 22:21:15 -0300

    added second feed for light sensor data

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 
 // visit io.adafruit.com if you need to create an account,
 // or if you need your Adafruit IO key.
-#define IO_USERNAME    "user_name"
+#define IO_USERNAME    "user"
 #define IO_KEY         "key"
-#define ADAFRUIT_IO_FEED "feed"
+#define ADAFRUIT_IO_DIMMER_FEED "feed"
+#define ADAFRUIT_IO_LDR_FEED "feed"
